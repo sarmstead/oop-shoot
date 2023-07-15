b8d93e54b448b77b8a71e4494d875f5b219414f3
Sunjay Armstead <connect@sunjayarmstead.local>
2023-07-15 16:29:57 -0400

    Add instructional comments

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -4,12 +4,12 @@ export default class Game {
   constructor(canvas) {
     this.canvas = canvas;
     this.context = canvas.getContext('2d');
-    this.circle = new Circle(this.context, canvas.width + 100, 200, 70);
+    this.circle = new Circle(this.context, canvas.width + 100, 200, 70); // circle is initialized outside of the canvas bounding box
   }
 
   update() {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.circle.x -= 1;
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); // clear all the pixels of the canvas, starting at grid points 0, 0
+    this.circle.x -= 1; // every time update() is called, subtract 1 from its current x value
     if (this.circle.x < -100) {
       this.circle.x = this.canvas.width + 100;
     }
@@ -17,6 +17,6 @@ export default class Game {
   }
 
   start() {
-    setInterval(this.update.bind(this), 1000 / 60);
+    setInterval(this.update.bind(this), 1000 / 60); // setInterval repeatedly calls the update() function every 1000 / 60 milliseconds
   }
 }
