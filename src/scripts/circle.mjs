757de7f217db9dd492440caa9acbf3e262908266
Sunjay Armstead <connect@sunjayarmstead.local>
2023-07-17 09:30:38 -0400

    Fix keyhandler for up and down arrows. Refactor Game and Circle

--- src/scripts/circle.mjs
+++ src/scripts/circle.mjs
@@ -6,13 +6,11 @@ export default class Circle {
     this.radius = radius;
   }
 
-  draw(deltaX, deltaY) {
+  draw() {
     this.context.beginPath();
     this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-    this.context.moveTo(this.x + deltaX, this.y + deltaY)
     this.context.fillStyle = 'green';
     this.context.fill();
-    this.context.stroke();
     this.context.closePath()
   }
 }
--- src/scripts/game.mjs
+++ src/scripts/game.mjs
@@ -1,37 +1,53 @@
 import Circle from './circle.mjs';
 import { Hero, Enemy } from './player.mjs';
 
+let downPressed = false
+let upPressed = false
+let radius = 30
+let changeInY = 10
 export default class Game {
   constructor(canvas) {
     this.canvas = canvas;
     this.context = canvas.getContext('2d');
-    this.circle = new Circle(this.context, - 70, canvas.height / 2, 70); // circle is initialized outside of the canvas bounding box
+    this.circle = new Circle(this.context, - 70, canvas.height / 2, radius); // circle is initialized outside of the canvas bounding box
+
   }
 
   update() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); // clear all the pixels of the canvas, starting at grid points 0, 0
-    this.circle.x += 1; // every time update() is called, subtract 1 from its current x value
-    if (this.circle.x > this.canvas.width + 70) {
-      this.circle.x = - 70;
+    this.circle.x += 5; // every time update() is called, add 5 to its current x value
+    if (this.circle.x > this.canvas.width + radius) {
+      this.circle.x = - radius;
+    }
+
+    if (downPressed) {
+      this.circle.y = Math.min(this.circle.y + changeInY, this.canvas.height - radius)
+    } else if (upPressed) {
+      this.circle.y = Math.max(this.circle.y - changeInY, radius)
     }
 
     this.circle.draw();
   }
 
-  move(event) {
-    switch(event.key) {
-      case 'ArrowUp':
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-        this.circle.y -= 30
-      case 'ArrowDown':
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-        this.circle.y += 30
-      }
-      this.circle.draw();
+  keydownHandler(event) {
+    if (event.key === 'Up' || event.key === 'ArrowUp') {
+      upPressed = true
+    } else if (event.key === 'Down' || event.key === 'ArrowDown') {
+      downPressed = true
+    }
+  }
+
+  keyupHandler(event) {
+    if (event.key === 'Up' || event.key === 'ArrowUp') {
+      upPressed = false
+    } else if (event.key === 'Down' || event.key === 'ArrowDown') {
+      downPressed = false
+    }
   }
 
   start() {
-  setInterval(this.update.bind(this), 2); // setInterval repeatedly calls the update() function every 1000 / 60 milliseconds
-    document.addEventListener('keydown', (event) => this.move(event), false)
+    setInterval(this.update.bind(this), 10); // setInterval repeatedly calls the update() function every 10 milliseconds
+    document.addEventListener('keydown', this.keydownHandler, false)
+    document.addEventListener('keyup', this.keyupHandler, false)
   }
 }
