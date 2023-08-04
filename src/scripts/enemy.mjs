6cf876b2e8e948e85d71bb626290a68128300442
Sunjay Armstead <connect@sunjayarmstead.local>
2023-08-04 18:02:09 -0400

    Add collision detection and game over state.

--- src/scripts/enemy.mjs
+++ src/scripts/enemy.mjs
@@ -1,6 +1,6 @@
-import Circle from './circle.mjs'
+import Image from './image.mjs'
 
-export default class Enemy extends Circle {
+export default class Enemy extends Image {
   constructor(...base) {
     super(...base)
     this.x = this.canvas.width + 100
@@ -8,7 +8,7 @@ export default class Enemy extends Circle {
 
   draw() {
     this.x -= 5
-    if (this.x < -this.radius) {
+    if (this.x < -this.width) {
       this.x = this.canvas.width + 100
     }
 
--- src/scripts/game.mjs
+++ src/scripts/game.mjs
@@ -8,18 +8,14 @@ export default class Game {
   constructor(canvas) {
     this.canvas = canvas;
     this.context = canvas.getContext('2d')
-    this.player = new Player('image__player', 438, 150, canvas, 30, 100)
-    this.enemy = new Enemy(radius, canvas, 30, 100)
-    this.objects = [this.player, this.enemy]
-  }
-
-  addObject(obj) {
-    this.objects.push(obj)
+    this.player = new Player('image__player', 438, 150, canvas, 10, this.canvas.height-150)
+    this.enemySpaghetti = new Enemy('image__enemy--spaghetti', 200, 200, canvas, 30, 0)
+    this.enemyBloat = new Enemy('image__enemy--bloat', 200, 200, canvas, 30, this.canvas.height-200)
+    this.objects = [this.player, this.enemySpaghetti, this.enemyBloat]
   }
 
   update() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); // clear all the pixels of the canvas, starting at grid points 0, 0
-
 
     if (downPressed) {
      this.player.moveDown()
@@ -27,8 +23,29 @@ export default class Game {
       this.player.moveUp()
     }
 
-
     this.objects.forEach(obj => obj.draw())
+    this.checkDamage(this.enemySpaghetti)
+    this.checkDamage(this.enemyBloat)
+  }
+
+  checkDamage(enemy) {
+    const topOfEnemy = enemy.y
+    const bottomOfEnemy = topOfEnemy - enemy.height
+    const topOfPlayer = this.player.y
+    const bottomOfPlayer = topOfPlayer - this.player.height
+    const leftEdgeOfEnemy = enemy.x
+    const rightEdgeOfEnemy = leftEdgeOfEnemy + enemy.width
+    const leftEdgeOfPlayer = this.player.x
+    const rightEdgeOfPlayer = leftEdgeOfPlayer + this.player.width
+
+    const horizontal = leftEdgeOfEnemy <= rightEdgeOfPlayer && rightEdgeOfEnemy >= leftEdgeOfPlayer
+    const vertical = bottomOfEnemy <= topOfPlayer && topOfEnemy >= bottomOfPlayer
+
+    if (vertical && horizontal) {
+      this.stop()
+    }
+
+    return
   }
 
   keydownHandler(event) {
@@ -47,8 +64,17 @@ export default class Game {
     }
   }
 
+  stop() {
+    clearInterval(this.startInterval)
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.font = '48px sans-serif'
+    this.context.textAlign = 'center'
+    this.context.fillText('GAME OVER', this.canvas.width / 2, this.canvas.height / 2)
+  }
+
   start() {
-    setInterval(this.update.bind(this), 10); // setInterval repeatedly calls the update() function every 10 milliseconds
+    this.startInterval = setInterval(this.update.bind(this), 10); // setInterval repeatedly calls the update() function every 10 milliseconds
+    this.startInterval
     document.addEventListener('keydown', this.keydownHandler, false)
     document.addEventListener('keyup', this.keyupHandler, false)
   }
